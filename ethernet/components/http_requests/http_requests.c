d3fbcc8569116a72df27fdae7e8283dab2c47756
spyrex <69646109+Band3er@users.noreply.github.com>
2025-06-05 20:48:53 +0300

    added http request

---
+++ ethernet/components/http_requests/http_requests.c
@@ -0,0 +1,176 @@
+#include "http_requests.h"
+
+
+void http_get_task(deviceInfo device)
+{
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
+    int s, r;
+    char recv_buf[64];
+
+    while(1) {
+        int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
+
+        if(err != 0 || res == NULL) {
+            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        /* Code to print the resolved IP.
+
+           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
+        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+        s = socket(res->ai_family, res->ai_socktype, 0);
+        if(s < 0) {
+            ESP_LOGE(TAG, "... Failed to allocate socket.");
+            freeaddrinfo(res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... allocated socket");
+
+        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+            close(s);
+            freeaddrinfo(res);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        ESP_LOGI(TAG, "... connected");
+        freeaddrinfo(res);
+
+        if (write(s, REQUEST, strlen(REQUEST)) < 0) {
+            ESP_LOGE(TAG, "... socket send failed");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... socket send success");
+
+        struct timeval receiving_timeout;
+        receiving_timeout.tv_sec = 5;
+        receiving_timeout.tv_usec = 0;
+        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                sizeof(receiving_timeout)) < 0) {
+            ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... set socket receiving timeout success");
+
+        /* Read HTTP response */
+        do {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf)-1);
+            for(int i = 0; i < r; i++) {
+                putchar(recv_buf[i]);
+            }
+        } while(r > 0);
+
+        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
+        close(s);
+        for(int countdown = 10; countdown >= 0; countdown--) {
+            ESP_LOGI(TAG, "%d... ", countdown);
+            //vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        ESP_LOGI(TAG, "Starting again!");
+        break;
+    }
+}
+
+void http_post_task(deviceInfo device)
+{
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
+    int s;
+    char recv_buf[128];
+
+    while (1) {
+        int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);  // Schimbă IP-ul cu cel al serverului tău
+        if (err != 0 || res == NULL) {
+            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+        s = socket(res->ai_family, res->ai_socktype, 0);
+        if (s < 0) {
+            ESP_LOGE(TAG, "Failed to allocate socket.");
+            freeaddrinfo(res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        if (connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+            ESP_LOGE(TAG, "Socket connect failed errno=%d", errno);
+            close(s);
+            freeaddrinfo(res);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        freeaddrinfo(res);
+
+        // Mesajul JSON pe care îl trimitem
+        //char *post_data = "{online:" +  device.online + "ip: , mac:}";
+
+        // Construim cererea HTTP POST
+        char request[256];
+        snprintf(request, sizeof(request),
+                 "POST /data HTTP/1.1\r\n"
+                 "Host: %s:%s\r\n"
+                 "Content-Type: application/json\r\n"
+                 "Content-Length: %d\r\n"
+                 "\r\n"
+                 "%s",WEB_SERVER, WEB_PORT,
+                 strlen(device), device);
+
+        if (write(s, request, strlen(request)) < 0) {
+            ESP_LOGE(TAG, "Socket send failed");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "Socket send success");
+
+        // Citim răspunsul serverului
+        int r;
+        do {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf) - 1);
+            if (r > 0) {
+                recv_buf[r] = '\0';
+                ESP_LOGI(TAG, "Received: %s", recv_buf);
+            }
+        } while (r > 0);
+
+        close(s);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        break;
+    }
+}
+
+void http_get_post_task(void *pvParameters){
+    while(1){
+        //http_get_task();
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        //http_post_task();
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
---
+++ ethernet/components/http_requests/http_requests.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_mac.h"
+#include "esp_eth.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+#include "scan.h"
+
+#define WEB_SERVER "192.168.100.3"  // change based on the laptop ip
+#define WEB_PORT "3000"
+#define WEB_PATH "/data"
+
+// test internet conenction
+//#define WEB_SERVER "httpbin.org"
+//#define WEB_PORT "80"
+//#define WEB_PATH "/"
+
+#define TAG "http_requests"
+
+static const char *REQUEST = "GET " WEB_PATH " HTTP/1.0\r\n"
+    "Host: "WEB_SERVER":"WEB_PORT"\r\n"
+    "User-Agent: esp-idf/1.0 esp32\r\n"
+    "\r\n";
+
+static void http_get_task(deviceInfo device);
+static void http_post_task(deviceInfo device);
+void http_get_post_task(void *pvParameters);
