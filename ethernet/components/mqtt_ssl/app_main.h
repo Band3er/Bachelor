8af629eb0a2c05d7ad04e2707a1d1abebfa8aaaa
spyrex <69646109+Band3er@users.noreply.github.com>
2025-06-23 00:37:52 +0300

    added mqtt functionality

---
+++ ethernet/components/mqtt_ssl/app_main.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+//#include "protocol_examples_common.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+#include "cJSON.h"
+
+#define TAG "mqtts_example"
+
+void mqtt_app_start(void);
+
+char* get_mqtt_data(void);
+
+bool is_mqtt_data_available(void);
+
+static SemaphoreHandle_t xSemaphore_Task;
+
